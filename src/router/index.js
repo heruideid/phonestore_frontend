41048603f5d4064e9f84af69179f0d02419d8da2
heruideid <2641180078@qq.com>
2020-07-16 09:26:49 +0800

    将前后端对接的api中ip换了

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddressList from "../views/AddressList";
+import AddressNew from "../views/AddressNew";
+import AddressEdit from "../views/AddressEdit";
+import Detail from "../views/Detail";
+import Success from "../views/Success";
+import Info from "../views/Info";
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/addressList',
+        name: "addressList",
+        component: AddressList
+    },
+    {
+        path: '/addressNew',
+        name: 'addressNew',
+        component: AddressNew
+    },
+    {
+        path: '/addressEdit',
+        name: 'addressEdit',
+        component: AddressEdit
+    },
+    {
+        path: "/detail",
+        name: "detail",
+        component: Detail
+    },
+    {
+        path: "/success",
+        name: "success",
+        component: Success
+    },
+    {
+        path: "/info",
+        name: "info",
+        component: Info
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
